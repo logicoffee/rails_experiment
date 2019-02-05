d5861e001c238e8d7d2c62863992fd85283862b5
logicoffee <riemann1618@gmail.com>
2019-02-05 13:51:03 +0900

    Specify scoped associations

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,3 +1,10 @@
 class Team < ApplicationRecord
+  # ノーマル
   has_many :players
+
+  # デフォルトで条件指定
+  # has_many :players, -> { where(deleted: false) }
+
+  # いつも使うとは限らない条件の場合は
+  # has_many :existing_players, -> { where(deleted: false) }, class_name: 'Player'
 end
